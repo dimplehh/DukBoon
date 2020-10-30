d9e8527f6ce4c47c7297693d5025d23db3013ff5
dimplehh <dimplehh@naver.com>
2020-10-30 21:37:08 +0900

    Get/Post 내용정리

--- week-3/get_post_ex.js
+++ week-3/get_post_ex.js
@@ -1,17 +1,18 @@
-const http = require('http');
-const url = require('url');
+const http = require('http');//http요청
+const url = require('url');//url요청
 
 http.createServer((req, res) => {
-  const urlObj = url.parse(req.url, true);
-  const pathName = urlObj.pathname;
-  if (pathName === '/') {
-    if (urlObj.search === null) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+  const urlObj = url.parse(req.url, true);//첫번째인자로 url,두번째인자로 boolean(true:객체,false:문자열)
+  const pathName=urlObj.pathname;//명령프롬프트에 pathname나오게 됨
+  console.log(urlObj);
+  if(pathName=='/'){
+    if (urlObj.search===null){
+      res.writeHead(200,{'Content-Type':'text/html'});
       res.write(`
       <html>
       <head>
         <meta charset="utf-8">
-        <title>GET / POST 메소드 실험</title>
+        <title>GET/POST 메소드 실험</title>
       </head>
       <body>
         <form action="/get" method="get">
@@ -25,44 +26,39 @@ http.createServer((req, res) => {
           <input type="submit" value="POST">
         </form>
       </body>
-    </html>
+      </html>
       `);
       res.end();
     }
-  } else if (pathName === '/get') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>GET / POST 메소드 실험</title>
-    </head>
-    <body>
-      <h1>GET!</h1>
-      <p>URL을 확인해 보세요.</p>
-    </body>
-  </html>
-`);
-    console.log(urlObj.query);
-    res.end();
-  } else if (pathName === '/post') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>GET / POST 메소드 실험</title>
-    </head>
-    <body>
-      <h1>POST!</h1>
-      <p>URL을 확인해 보세요.</p>
-    </body>
-  </html>
-`);
-    console.log(urlObj.query);
-    res.end();
-  } else {
-    res.writeHead(404);
-    res.end();
+  }else if (pathName==='/get'){
+    res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(`
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <title>GET/POST 메소드 실험</title>
+      </head>
+      <body>
+        <h1>GET!</h1>
+        <p>URL을 확인해 보세요.</p>
+      </body>
+      </html>
+      `);
+      res.end();//res.end잊지말기.get 쓸 때는 text내용까지 나옴
+  }else if (pathName==='/post'){
+    res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(`
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <title>GET/POST 메소드 실험</title>
+      </head>
+      <body>
+        <h1>POST!</h1>
+        <p>URL을 확인해 보세요.</p>
+      </body>
+      </html>
+      `);
+      res.end();//res.end잊지말기 post쓸때는 form데이터에서만 내용 나옴
   }
-}).listen(3000);
+}).listen(3000);//3000번포트로
